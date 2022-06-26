25a039e1ed872de8016f43135e84bd387e6df1b7
PedroTeloeken <pedroteloeken2211@gmail.com>
2022-06-25 22:17:26 -0300

    Add uma nova activity

---
+++ app/src/main/java/com/example/covidados2/ui/GuestFormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.covidados2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covidados2.R
+
+class GuestFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest_form)
+    }
+}
