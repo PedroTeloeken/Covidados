3d5f5e89287150278587b208fda85a4b2f11cb20
PedroTeloeken <pedroteloeken2211@gmail.com>
2022-06-27 00:03:36 -0300

    Conectando as classes

--- app/src/main/java/com/example/covidados2/ui/ViewModel/GuestFormViweModel.kt
+++ app/src/main/java/com/example/covidados2/ui/ViewModel/GuestFormViweModel.kt
@@ -1,10 +1,12 @@
 package com.example.covidados2.ui.ViewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.example.covidados2.ui.repository.GuestRepository
 
-class GuestFormViweModel : ViewModel() {
+class GuestFormViweModel(application: Application) : AndroidViewModel(application) {
 
-    //private val repository = GuestRepository()
+    private val repository = GuestRepository.getInstance(application)
 
 }
